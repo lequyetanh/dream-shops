f94f165d5e83a9c5cb232df5292688e5a0a84248
lequyetanh <73962957+lequyetanh@users.noreply.github.com>
2025-12-31 20:13:53 +0700

    add mapstruct

---
+++ src/main/java/com/dailycodework/dreamshops/mapper/category/CategoryMapper.java
@@ -0,0 +1,7 @@
+package com.dailycodework.dreamshops.mapper.category;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
+public interface CategoryMapper {
+}
