b8bc54d7f95c412b4d3dec261763c5e456a79c00
lequyetanh <73962957+lequyetanh@users.noreply.github.com>
2026-05-28 22:03:31 +0700

    add new tuple mapper

--- src/main/java/com/dailycodework/dreamshops/repository/warehouseTransaction/IWarehouseTransactionRepositoryImpl.java
+++ src/main/java/com/dailycodework/dreamshops/repository/warehouseTransaction/IWarehouseTransactionRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.dailycodework.dreamshops.repository.warehouseTransaction;
 
 import com.dailycodework.dreamshops.payload.dto.warehouseTransaction.WarehouseTransactionList;
 import com.dailycodework.dreamshops.util.Common;
+import com.dailycodework.dreamshops.util.TupleMapper;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.Query;
 import jakarta.persistence.Tuple;
@@ -66,20 +67,67 @@ public class IWarehouseTransactionRepositoryImpl implements WarehouseTransaction
                 Tuple.class
         );
         Common.setParamsWithPageable(query, params, pageable, 0);
-        List<Tuple> tuples = query.getResultList();
-        warehouseTransactionLists = tuples.stream().map(tuple -> {
-            WarehouseTransactionList warehouseTransactionList = new WarehouseTransactionList();
-            warehouseTransactionList.setId(tuple.get("id", Long.class));
-            warehouseTransactionList.setCompanyId(tuple.get("companyId", Long.class));
-            warehouseTransactionList.setNo(tuple.get("no", String.class));
-            warehouseTransactionList.setDate(tuple.get("date", ZonedDateTime.class));
-            warehouseTransactionList.setDescription(tuple.get("description", String.class));
-            warehouseTransactionList.setAmount(tuple.get("amount", BigDecimal.class));
-            warehouseTransactionList.setVatRate(tuple.get("vatRate", Integer.class));
-            warehouseTransactionList.setVatAmount(tuple.get("vatAmount", BigDecimal.class));
-            warehouseTransactionList.setTotalAmount(tuple.get("totalAmount", BigDecimal.class));
-            return warehouseTransactionList;
-        }).collect(Collectors.toList());
-        return new PageImpl<>(warehouseTransactionLists, pageable, totalItem);
+
+//        List<Tuple> tuples = query.getResultList();
+//        warehouseTransactionLists = tuples.stream().map(tuple -> {
+//            WarehouseTransactionList warehouseTransactionList = new WarehouseTransactionList();
+//            warehouseTransactionList.setId(tuple.get("id", Long.class));
+//            warehouseTransactionList.setCompanyId(tuple.get("companyId", Long.class));
+//            warehouseTransactionList.setNo(tuple.get("no", String.class));
+//            warehouseTransactionList.setDate(tuple.get("date", ZonedDateTime.class));
+//            warehouseTransactionList.setDescription(tuple.get("description", String.class));
+//            warehouseTransactionList.setAmount(tuple.get("amount", BigDecimal.class));
+//            warehouseTransactionList.setVatRate(tuple.get("vatRate", Integer.class));
+//            warehouseTransactionList.setVatAmount(tuple.get("vatAmount", BigDecimal.class));
+//            warehouseTransactionList.setTotalAmount(tuple.get("totalAmount", BigDecimal.class));
+//            return warehouseTransactionList;
+//        }).collect(Collectors.toList());
+//        return new PageImpl<>(warehouseTransactionLists, pageable, totalItem);
+
+        List<WarehouseTransactionList> results = TupleMapper.mapList(query, WarehouseTransactionList.class);
+        return new PageImpl<>(results, pageable, totalItem);
+    }
+
+    @Override
+    public Page<WarehouseTransactionList> getWarehouseTransactionWithPaging2(
+            Pageable pageable,
+            String keyword,
+            String fromDate,
+            String toDate,
+            Integer companyId
+    ){
+        Integer totalItem = 0;
+        StringBuilder countSql = new StringBuilder();
+        countSql.append("select count(*) from warehouse_transaction wt");
+        Query countQuery = entityManager.createNativeQuery(countSql.toString());
+        totalItem = (Integer) countQuery.getSingleResult();
+
+        List<WarehouseTransactionList> warehouseTransactionLists = new ArrayList<>();
+        Map<String, Object> params = new HashMap<>();
+        StringBuilder sql = new StringBuilder();
+        sql.append(" from warehouse_transaction wt left join warehouse_transaction_detail wtd on wt.id = wtd.warehouse_transaction_id ");
+        if(keyword != null && !keyword.isEmpty()){
+            sql.append(" where (wt.code like :keyword) ");
+            params.put("keyword", "%" + keyword + "%");
+        }
+        if(companyId != null){
+            sql.append(" and wt.company_id = :companyId ");
+            params.put("companyId", companyId);
+        }
+
+        Query query = entityManager.createNativeQuery(
+                "select " +
+                        "wt.id, " +
+                        "wt.company_id companyId, " +
+                        "wt.no, " +
+                        "wt.date, " +
+                        "wt.description" +
+                        sql,
+                Tuple.class
+        );
+        Common.setParamsWithPageable(query, params, pageable, 0);
+        List<WarehouseTransactionList> results = TupleMapper.mapList(query, WarehouseTransactionList.class);
+
+        return new PageImpl<>(results, pageable, totalItem);
     }
 }
